ec3cd89a20970ce58036220ef2d79970cad929b8
enunes1974 <emanuel.bettencourtnunes1974@gmail.com>
2026-08-30 12:23:48 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "mycrypto-v2";
-const SHELL = ["./index.html", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"];
+const CACHE = "mycrypto-v3";
+const SHELL = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)));
